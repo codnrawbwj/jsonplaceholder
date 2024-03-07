8fb1e1163deebd1e2bb1ade052b77e98444e0d7c
codnrawbwj <codnrawbwj@gmail.com>
2024-03-06 21:37:42 -0600

    implement gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,46 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useState } from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  console.log("app")
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
-    <Component {...pageProps}/>
+    <>
+      <Head>
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} 
+      />
+      <Component {...pageProps}/>
+    </>
   );
 }
